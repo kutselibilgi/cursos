b9edd0b267be4f865cc9ffd8d04435250289f6ad
Thiago Morais Araujo <t.moraisaraujo@gmail.com>
2023-02-22 10:53:42 -0300

    Ambiente funcionando

--- udemy_react_docker/1_INTRO/hello_world/src/App.js
+++ udemy_react_docker/1_INTRO/hello_world/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit2  Thiago <code>src/App.js</code> and save to reload.
+          Edit Thiago Morais <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

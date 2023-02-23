270db2a304f0c06d62483cb74e1e78cbcadd4e2e
Thiago Morais Araujo <t.moraisaraujo@gmail.com>
2023-02-23 19:46:03 -0300

    Correção do script docker para permitir acesso à porta externa

--- udemy_react_docker/dockerBash.sh
+++ udemy_react_docker/dockerBash.sh
@@ -1,7 +1,7 @@
 docker build -t ubuntu_node . &&
 docker run -it --name udemy_react_docker --rm \
 --volume $(pwd):/usr/src/udemy_react_docker \
---net=host ubuntu_node \
+-p 3000:3000 ubuntu_node \
 bash
 
 

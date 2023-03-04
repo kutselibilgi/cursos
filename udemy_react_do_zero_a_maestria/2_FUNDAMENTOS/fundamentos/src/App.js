40cfcfb1ffdd9f27a03504b3b1845c2eb3cc5f27
Thiago Morais Araujo <t.moraisaraujo@gmail.com>
2023-03-04 11:09:37 -0300

    Aula 22

--- udemy_react_do_zero_a_maestria/2_FUNDAMENTOS/fundamentos/src/App.js
+++ udemy_react_do_zero_a_maestria/2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,6 +1,7 @@
 //components
 import FirstComponent from "./components/FirstComponent.js"
 import TemplateExpressions from "./components/TemplateExpressions.js";
+import MyComponent from "./components/MyComponent.js";
 
 //styles / CSS
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <h1>Fundamentos React</h1>
         <FirstComponent />
         <TemplateExpressions />
+        <MyComponent />
     </div>
   );
 }
--- udemy_react_do_zero_a_maestria/2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ udemy_react_do_zero_a_maestria/2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,11 @@
+import MyComponent from "./MyComponent";
+
 const FirstComponent = () => {
     return (
         <div>
             <h1> Meu Primeiro Componente </h1>
             <p>Meu Texto</p>
+            <MyComponent />
         </div>
     );
 };

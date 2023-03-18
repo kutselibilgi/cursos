2e188b6b20eb5f5b5fe4e3aa57bb7e90390cd6e6
Thiago Morais Araujo <t.moraisaraujo@gmail.com>
2023-03-18 18:03:17 -0300

    Curso React - Aula 34

--- udemy_react_do_zero_a_maestria/3_AVANCANDO_EM_REACT/avancando_react/src/components/ListRender.js
+++ udemy_react_do_zero_a_maestria/3_AVANCANDO_EM_REACT/avancando_react/src/components/ListRender.js
@@ -2,23 +2,35 @@ import React, { useState } from 'react';
 
 const ListRender = () => {
   const [list] = useState(['Matheus', 'Pedro', 'Josias', 'Maria']);
-const [user, setuser] = useState([
-	{ id: 1, name: "Thiago", age: 37},
-	{id: 4324, name: "Amanda", age: 32},
-	{id: 2312321, name: "Felipe", age: 31},
-]);
+  const [user, setUser] = useState([
+    { id: 1, name: 'Thiago', age: 37 },
+    { id: 2, name: 'Amanda', age: 32 },
+    { id: 3, name: 'Felipe', age: 31 }
+  ]);
+
+  const deleteRandomUser = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    setUser((prevUser) => {
+      console.log(prevUser);
+      return prevUser.filter((user) => randomNumber !== user.id);
+    });
+  };
   return (
     <div>
       <ul>
-		{list.map((item,index) => (
-			<li key={index}>{item}</li>
-		))}
-	  </ul>
-	  <ul>{user.map((user) => (
-		<li key={user.id}>
-			{user.name} - {user.age}
-		</li>
-	  ))}</ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {user.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </ul>
+      <button onClick={deleteRandomUser}>Delete Random User</button>
     </div>
   );
 };

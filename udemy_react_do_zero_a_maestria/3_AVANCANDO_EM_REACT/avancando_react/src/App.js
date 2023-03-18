9c8359480e05a57d5ec2f57fd1049be07610fca2
Thiago Morais Araujo <t.moraisaraujo@gmail.com>
2023-03-18 10:13:55 -0300

    Curso React - aula 31

--- udemy_react_do_zero_a_maestria/3_AVANCANDO_EM_REACT/avancando_react/src/App.js
+++ udemy_react_do_zero_a_maestria/3_AVANCANDO_EM_REACT/avancando_react/src/App.js
@@ -1,9 +1,21 @@
+// imports
 import './App.css';
+import City from './assets/city.jpg'
+import ManageData from './components/ManageData';
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React!</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em assets */}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
+      <ManageData />
     </div>
   );
 }
